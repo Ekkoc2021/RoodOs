1eae3cbf461700a01305655aee12ac51ce9b3ae2
EKKO <189890049@qq.com>
2024-02-29 18:02:57 +0800

    普通文件相关读写操作

--- src/kernel/fs/regular.c
+++ src/kernel/fs/regular.c
@@ -1,1 +1,15 @@
 // 普通文件
+// 目录文件相关接口
+#include "fs.h"
+file_type regular_type = {
+    .type = FT_DIRECTORY,
+    .open = open_file,
+    .read = read_file,
+    .write = write_file,
+    .control = control_file,
+    .info = info_file,
+    .close = close_file,
+};
+void init_regular()
+{
+}
